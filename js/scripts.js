382e79c07df788994a5b4bbdef6ae88f1c62ca06
SydNichols <sydneynichols.m@gmail.com>
2025-03-19 09:35:07 -0400

    Added in API functionality
    Added a fetchData function to get random employee information.
    
    Added the displayEmployees function to include the employee data in the html

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+//initializing an empty employees array as a global variable 
+//establishing the location of the gallery div as a global variable
+
+let employees = [];
+const galleryDiv = document.getElementById('gallery');
+
+//fetchData function will be used to fetch the random employee from the random user API
+function fetchData() {
+    fetch('https://randomuser.me/api/?results=12&nat=us,gb,ca,au,nz')
+        .then(response => response.json())
+        .then(data => {
+            employees = data.results;
+            displayEmployees(employees);
+        })
+        .catch(error => console.log('Error fetching data:', error));
+}
+
+fetchData();
+
+//function used to temporarily test the return of the employee information
+/* 
+function displayEmployees(employeeData) {
+    console.log('Employee data received:', employeeData);
+} */
+
+
+//displayEmployees will take the fetched data and display it in the html 
+function displayEmployees(employeeData) {
+    galleryDiv.innerHTML = '';
+
+    employeeData.forEach(employee => {
+        const html = `
+            <div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="${employee.picture.large}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                    <p class="card-text">${employee.email}</p>
+                    <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+                </div>
+            </div>
+        `;
+
+        galleryDiv.insertAdjacentHTML('beforeend', html);
+    });
+}
