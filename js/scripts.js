9614f6a5510bee5c4c36ce4cc922d9bc5816c8f5
SydNichols <sydneynichols.m@gmail.com>
2025-03-19 10:07:34 -0400

    adding in the basic card functionality
    added in the displayModal function for displaying the card modal after the card is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -62,5 +62,29 @@ function cardListeners() {
 
 //display for when a card is clicked
 function displayModal(index) {
-    console.log('displayed index:', index)
+    //console.log('displayed index:', index)
+
+    const employee = employees[index];
+    
+
+    //display the modal after click
+    const modalHTML = `
+        <div class="moodal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                    <p class="modal-text">${employee.email}</p>
+                    <p class="modal-text cap">${employee.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${employee.cell}</p>
+                    <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${employee.dob.date}
+                </div>
+            </div>
+        </div>
+    `;
+
+    document.body.insertAdjacentHTML('beforeend', modalHTML);
 }
